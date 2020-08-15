ddfb1fc3736ea116a3da1f3a216dc142948b307b
Stefan <stefan.jovanavich@gmail.com>
2020-08-15 22:03:13 +0200

    ToDoDatabase singleton updated/fixed. navHostFragment's layout width and height are now 0dp (match constraint)

--- app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
@@ -9,29 +9,47 @@ import com.example.todoapp.data.models.ToDoData
 
 @Database(entities = [ToDoData::class], version = 1, exportSchema = false)
 @TypeConverters(Converter::class)
-abstract class ToDoDatabase: RoomDatabase() {
+abstract class ToDoDatabase : RoomDatabase() {
 
     abstract fun toDoDao(): ToDoDao
+
+//    companion object {
+//        @Volatile
+//        private var INSTANCE: ToDoDatabase? = null
+//
+//        fun getDatabase(context: Context): ToDoDatabase {
+//            val tempInstance = INSTANCE
+//            if (tempInstance != null) {
+//                return tempInstance
+//            }
+//            synchronized(this) {
+//                val instance = Room.databaseBuilder(
+//                    context.applicationContext,
+//                    ToDoDatabase::class.java,
+//                    "todo_database"
+//                ).build()
+//                INSTANCE = instance
+//                return instance
+//            }
+//        }
+//    }
 
     companion object {
         @Volatile
         private var INSTANCE: ToDoDatabase? = null
 
-        fun getDatabase(context: Context): ToDoDatabase {
-            val tempInstance = INSTANCE
-            if(tempInstance != null){
-                return tempInstance
+        fun getDatabase(context: Context): ToDoDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE
+                    ?: buildDatabase(context).also { INSTANCE = it }
             }
-            synchronized(this){
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    ToDoDatabase::class.java,
-                    "todo_database"
-                ).build()
-                INSTANCE = instance
-                return instance
-            }
-        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ToDoDatabase::class.java, "todo_database"
+            )
+                .build()
     }
 
 }
